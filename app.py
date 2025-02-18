e613eb464919e42fb9569774ef6d450ce82e854f
Kaushik <kaushiksamba@users.noreply.huggingface.co>
2025-02-18 18:39:55 +0000

    Add already-created tools to the agent

--- app.py
+++ app.py
@@ -55,7 +55,11 @@ with open("prompts.yaml", 'r') as stream:
     
 agent = CodeAgent(
     model=model,
-    tools=[final_answer], ## add your tools here (don't remove final answer)
+    tools=[
+        image_generation_tool, 
+        get_current_time_in_timezone,
+        final_answer,
+    ], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
